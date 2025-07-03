a2f48349eb4ee56154a1c2ed4e1bc6caa685ddd6
Jjansennn <jansenjansenkhang@gmail.com>
2025-07-03 23:52:10 +0700

    layout fragment status dan profile

---
+++ app/src/main/java/com/example/bengkelkuapp/ui/StatusDetailFragment.java
@@ -0,0 +1,41 @@
+package com.example.bengkelkuapp.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import com.example.bengkelkuapp.R;
+
+public class StatusDetailFragment extends Fragment {
+
+    public StatusDetailFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout
+        return inflater.inflate(R.layout.fragment_status_detail, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Navigasi dari LinearLayout repair ke StatusFragment
+        LinearLayout repairLayout = view.findViewById(R.id.repair);
+        if (repairLayout != null) {
+            repairLayout.setOnClickListener(v ->
+                    Navigation.findNavController(view).navigate(R.id.action_statusDetailFragment_to_statusFragment)
+            );
+        }
+    }
+}
